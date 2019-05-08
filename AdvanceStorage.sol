fb72231f9b232e82213b2a68b439850eb9f18877
sushba01 <sushba01@noa.nintendo.com>
2019-05-08 11:29:52 -0700

    Updated the format

--- AdvanceStorage.sol
+++ AdvanceStorage.sol
@@ -5,23 +5,23 @@ contract AdvanceStorage{
     uint arrayLength;
     
     function add(uint _data) public {
-        arrayLength=advStore.push(_data);
+        arrayLength = advStore.push(_data);
     }
     
-    function size()view public returns(uint){
+    function size()public view  returns(uint){
         return arrayLength;
     }
     
     function remove(uint _index) public {
-        if(_index > advStore.length -1) return;
+        if(_index > (advStore.length - 1)) return;
         delete advStore[_index];
     }
     
-    function get(uint _index) view public returns(uint){
+    function get(uint _index) public view returns(uint){
         return advStore[_index];
     }
     
-    function getAll() view public returns(uint[] memory){
+    function getAll() public view  returns(uint[] memory){
         return advStore;
     }
 }

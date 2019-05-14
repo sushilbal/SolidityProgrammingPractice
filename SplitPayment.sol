fd10b43551eb892237f2455f8009d66d7f53c5ac
Sushil Bal <sushil.kumar.bal@gmail.com>
2019-05-13 21:21:42 -0700

    Updated SplitPayment to Use Struct rather than Mapping as mapping is not iterable.

--- SplitPayment.sol
+++ SplitPayment.sol
@@ -1,28 +1,30 @@
 pragma solidity ^0.5.0;
 
 contract SplitPayment{
-    
+
     address owner;
-    mapping(address=> uint) toPay;
-    address payable[] toPayKey;
-    
-    constructor() public{
-        owner=msg.sender;
+    struct toPay{
+        address payable toUser;
+        uint amountToPay;
     }
-    
+    toPay[] toPayArray;
+
+    constructor() public{
+        owner = msg.sender;
+    }
+
     modifier ownerOnly(){
         require(msg.sender == owner,"Only owner is allowed.");
         _;
     }
-    
-    function addToBePaid(address payable _toBePaid ,uint amount)public ownerOnly{
-        toPay[_toBePaid]=amount;
-        toPayKey.push(_toBePaid);
+
+    function addToBePaid(address payable _toBePaid,uint amount)public ownerOnly{
+        toPayArray.push(toPay(_toBePaid,amount));
     }
-    
+
     function transferToEverybody() public ownerOnly payable{
-        for(uint i=0;i<toPayKey.length;i++){
-            toPayKey[i].transfer(toPay[toPayKey[i]]);
-        } 
+        for(uint i = 0;i < toPayArray.length;i++){
+            toPayArray[i].toUser.transfer(toPayArray[i].amountToPay);
+        }
     }
 }
